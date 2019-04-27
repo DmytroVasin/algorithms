cc0547111ed4706aa12f875c5321a0045ecf404b
Dmytro Vasin <dmytro.vasin@gmail.com>
2019-04-27 20:54:01 +0300

    231

--- tasks/leetcode/231-power-of-two.rb
+++ tasks/leetcode/231-power-of-two.rb
@@ -11,3 +11,13 @@ def is_power_of_two(n)
 
   true
 end
+
+
+def is_power_of_two(n)
+  return false if n <= 0
+
+  bits = n.to_s(2)
+  return false if !bits.start_with?('1')
+
+  bits.count('1') == 1
+end
